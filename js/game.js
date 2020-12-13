02bcfc10097e0d64be784d609c383fbc621b85db
hzllzh <1160313432@qq.com>
2020-12-13 22:55:30 +0800

    full proj

--- js/game.js
+++ js/game.js
@@ -1,33 +1,25 @@
 (function () {
+    //搞定轮播图
     //轮播图的数据
-    var carouselDatas = [{
-            image: "https://game.gtimg.cn/images/lolm/m/f_1.jpg",
+    var carouselData = [
+        {
+          image: "https://game.gtimg.cn/images/lolm/m/f_1.jpg",
         },
         {
-            image: "https://game.gtimg.cn/images/lolm/m/f_2.jpg",
+          image: "https://game.gtimg.cn/images/lolm/m/f_2.jpg",
         },
         {
-            image: "https://game.gtimg.cn/images/lolm/m/f_3.jpg",
+          image: "https://game.gtimg.cn/images/lolm/m/f_3.jpg",
         },
-        {
-            image: "https://game.gtimg.cn/images/lolm/m/f_4.jpg",
+          {
+          image: "https://game.gtimg.cn/images/lolm/m/f_4.jpg",
         },
-        {
-            image: "https://game.gtimg.cn/images/lolm/m/f_5.jpg",
+          {
+          image: "https://game.gtimg.cn/images/lolm/m/f_5.jpg",
         },
-        {
-            image: "https://game.gtimg.cn/images/lolm/m/f_6.jpg",
+          {
+          image: "https://game.gtimg.cn/images/lolm/m/f_6.jpg",
         },
-    ];
-    createCarousel("gameCarousel", carouselDatas);
-
-    var container = $(".game_container");
-
-    container.ontouchstart = function(e){
-        console.log(container.scrollTop);
-        if(container.scrollTop >= 10){ //滑动条的位置
-            //滑动的位置已经不在顶部了
-            e.stopPropagation();//阻止事件冒泡
-        }
-    };
+      ];
+    createCarousel("gameCarousel", carouselData);
 })();
